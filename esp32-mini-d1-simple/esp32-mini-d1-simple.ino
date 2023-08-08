5565328de37dbc1a2fb1a9775e00dceea1a4c765
David Perales Ferrera <dperalesferrera@gmail.com>
2023-08-08 16:55:04 +0200

    no message

--- esp32-mini-d1-simple/esp32-mini-d1-simple.ino
+++ esp32-mini-d1-simple/esp32-mini-d1-simple.ino
@@ -6,6 +6,10 @@
 #include <Bounce2.h>
 #include <BleKeyboard.h>
 
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
 // DeviceName, DeviceManufacturer, BatteryLevel
 BleKeyboard bleKeyboard("Mando Sadrollas", "Sadrollas Corp, 100");
 
@@ -18,9 +22,12 @@ int map_left = 17;
 int go_location = 2;
 int pulse = 150; //Tiempo de espera para repetir al mantener pulsado
 
-// Configure en 0 para ver todas las direcciones de dispositivos vinculados, configure en 1 para eliminar
+// Set to 0 to view all bonded devices addresses, set to 1 to remove
 #define REMOVE_BONDED_DEVICES 0
-#define PAIR_MAX_DEVICES 9
+//The NimBLE mode enables a significant saving of RAM and FLASH memory
+#define USE_NIMBLE
+
+#define PAIR_MAX_DEVICES 20
 uint8_t pairedDeviceBtAddr[PAIR_MAX_DEVICES][6];
 char bda_str[18];
 
@@ -36,12 +43,13 @@ const uint8_t BUTTON_PINS[NUM_BUTTONS] = {
 };
 
 Bounce * buttons = new Bounce[NUM_BUTTONS];
-unsigned long buttonPressTimeStampu;
-unsigned long buttonPressTimeStampd;
+
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
-  checkReset();
+  Serial.println("Starting BT Device!!!");
+  //initBluetooth();
+  //checkReset();
   bleKeyboard.begin();
   for (int i = 0; i < NUM_BUTTONS; i++) {
     //setup the bounce instance for the current button
@@ -52,54 +60,57 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    //Update the Bounce instance
-    buttons[i].update();
-  }
-
-  // fell() => Returns true if pin signal transitions from low to high
-  if (buttons[0].fell()) {
-    Serial.println("go location");
-    if (bleKeyboard.isConnected()) {
-      bleKeyboard.write(67); // El 67 es la C del teclado decimal
+  if(bleKeyboard.isConnected()) {
+    for (int i = 0; i < NUM_BUTTONS; i++) {
+      //Update the Bounce instance
+      buttons[i].update();
     }
-  }
 
-  if (buttons[1].fell()) {
-    Serial.println("map top");
-    if (bleKeyboard.isConnected()) {
-      bleKeyboard.write(KEY_UP_ARROW);
+    // fell() => Returns true if pin signal transitions from low to high
+    if (buttons[0].fell()) {
+      Serial.println("go location");
+      if (bleKeyboard.isConnected()) {
+        bleKeyboard.write(67); // El 67 es la C del teclado decimal
+      }
     }
-  }
 
-  if (buttons[2].fell()) {
-    Serial.println("map down");
-    if (bleKeyboard.isConnected()) {
-      bleKeyboard.write(KEY_DOWN_ARROW);
+    if (buttons[1].fell()) {
+      Serial.println("map top");
+      if (bleKeyboard.isConnected()) {
+        bleKeyboard.write(KEY_UP_ARROW);
+      }
     }
-  }
 
-  if (digitalRead(zoom_in) == LOW) {
-    Serial.println("zoom in");
-    bleKeyboard.write(43); // el 43 es la tecla + del teclado para el zoom out
-    delay(pulse);
-  }
-  else if (digitalRead(zoom_out) == LOW) {
-    Serial.println("zoom out");
-    bleKeyboard.write(45); //el  45 es la tecla - para zoom in
-    delay(pulse);
-  }
-  else if (digitalRead(map_right) == LOW) {
-    Serial.println("map right");
-    bleKeyboard.write(KEY_RIGHT_ARROW);
-    delay(pulse);
-  }
-  else if (digitalRead(map_left) == LOW) {
-    Serial.println("map left");
-    bleKeyboard.write(KEY_LEFT_ARROW);
-    delay(pulse);
+    if (buttons[2].fell()) {
+      Serial.println("map down");
+      if (bleKeyboard.isConnected()) {
+        bleKeyboard.write(KEY_DOWN_ARROW);
+      }
+    }
+
+    if (digitalRead(zoom_in) == LOW) {
+      Serial.println("zoom in");
+      bleKeyboard.write(43); // el 43 es la tecla + del teclado para el zoom out
+      delay(pulse);
+    }
+    else if (digitalRead(zoom_out) == LOW) {
+      Serial.println("zoom out");
+      bleKeyboard.write(45); //el  45 es la tecla - para zoom in
+      delay(pulse);
+    }
+    else if (digitalRead(map_right) == LOW) {
+      Serial.println("map right");
+      bleKeyboard.write(KEY_RIGHT_ARROW);
+      delay(pulse);
+    }
+    else if (digitalRead(map_left) == LOW) {
+      Serial.println("map left");
+      bleKeyboard.write(KEY_LEFT_ARROW);
+      delay(pulse);
+    }
   }
 }
+
 
 bool initBluetooth() {
   if (!btStart()) {
@@ -116,6 +127,7 @@ bool initBluetooth() {
     Serial.println("Failed to enable bluedroid");
     return false;
   }
+  
   return true;
 }
 
@@ -129,23 +141,22 @@ char * bda2str(const uint8_t * bda, char * str, size_t size) {
 }
 
 void checkReset() {
-  initBluetooth();
   Serial.print("ESP32 bluetooth address: ");
   Serial.println(bda2str(esp_bt_dev_get_address(), bda_str, 18));
   // Get the numbers of bonded/paired devices in the BT module
   int count = esp_bt_gap_get_bond_device_num();
   if (!count) {
-    Serial.println("No bonded device found.");
+    Serial.println("No bonded BT device found.");
   }
   else {
     Serial.print("Bonded device count: ");
     Serial.println(count);
     if (abs(count) > PAIR_MAX_DEVICES) {
-      //count = PAIR_MAX_DEVICES;
+      count = PAIR_MAX_DEVICES;
       Serial.print("Max pair devices exceed... resetting bonded devices");
-      REMOVE_BONDED_DEVICES == 0;
+      Serial.println(count);
     }
-    esp_err_t tError = esp_bt_gap_get_bond_device_list( & count, pairedDeviceBtAddr);
+    esp_err_t tError = esp_bt_gap_get_bond_device_list(&count, pairedDeviceBtAddr);
     if (ESP_OK == tError) {
       for (int i = 0; i < count; i++) {
         Serial.print("Found bonded device # ");
@@ -158,7 +169,7 @@ void checkReset() {
             Serial.print("Removed bonded device # ");
           }
           else {
-            Serial.print("Failed to remove bonded device # ");
+            Serial.print("Failed to remove bonded BT device # ");
           }
           Serial.println(i);
         }
